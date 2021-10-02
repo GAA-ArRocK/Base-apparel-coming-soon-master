d94951ffed9cf4b4c7b3904670b002f359ff2a1f
ArRocK <arrock@mail.ru>
2021-10-02 16:47:16 +0300

    Mobile part ready (no optimization)

---
+++ script.js
@@ -0,0 +1,13 @@
+function mailCheck(inputMail) {
+    const errorMessage = document.getElementsByClassName("error")[0];
+    const mailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    if (inputMail.match(mailFormat)) {
+        errorMessage.classList.remove("active");
+        document.checkForm.email.value = ""
+        alert("Email " + inputMail + " has been added to the list. Thank you for participation!");
+    }
+    else {
+        errorMessage.classList.add("active");
+    }
+}
